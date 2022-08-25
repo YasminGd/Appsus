1aae2086796129270084276bbe4c543ffe225afe
ohayonb <ohayonzzb@gmail.com>
2022-08-25 19:04:47 +0300

    mail-details cmp started

--- apps/mail/views/mail-details.jsx
+++ apps/mail/views/mail-details.jsx
@@ -1,7 +1,19 @@
 import { MailFolderList } from '../cmps/mail-folder-list.jsx'
+import { mailService } from '../services/mail.service.js'
 
 export class MailDetails extends React.Component {
+  state = {
+    mail: null,
+  }
+
+  componentDidMount() {
+    console.log(`mailService:`, mailService)
+  }
+
+  loadMail = () => {}
+
   render() {
+    console.log(`this.props:`, this.props)
     return (
       <section className="mail-details">
         <MailFolderList />
