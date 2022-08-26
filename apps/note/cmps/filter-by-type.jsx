e244ff8f6e2ed33037dd749a219f2d2628e8d48a
yasmin <thegodha1@gmail.com>
2022-08-26 23:17:29 +0300

    perfected the layout

---
+++ apps/note/cmps/filter-by-type.jsx
@@ -0,0 +1,8 @@
+export function FilterByType() {
+    return <section className="filter-by-type">
+        <button className="filter-container" ><i class="fa-solid fa-font"></i></button>
+        <button className="filter-container" ><i class="fa-brands fa-youtube"></i></button>
+        <button className="filter-container" ><i class="fa-solid fa-image"></i></button>
+        <button className="filter-container" ><i class="fa-solid fa-list"></i></button>
+    </section>
+}
