83ce0222c313ee56f9ee4dea3b5922b8ce98cb23
yasmin <thegodha1@gmail.com>
2022-08-27 17:34:49 +0300

    fixed broken image paths

--- apps/note/cmps/note-pin.jsx
+++ apps/note/cmps/note-pin.jsx
@@ -2,7 +2,7 @@ export function NotePin({ onTogglePinnedNote, isPinned, noteId }) {
     const imgSrc = isPinned ? 'pinned-note.svg' : 'unpinned-note.svg'
 
     return <div className="note-pin" onClick={(ev) => onTogglePinnedNote(ev, noteId)} >
-        <img src={`./../../assets/img/note/${imgSrc}`} alt="" />
+        <img src={`./assets/img/note/${imgSrc}`} alt="" />
     </div>
 
 }
